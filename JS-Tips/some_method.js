92682f716934be69e5ad2892c366072f749f0eee
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-05-05 23:38:03 +0300

    Tip on some method in js arrays

---
+++ JS-Tips/some_method.js
@@ -0,0 +1,16 @@
+const array = ["hello", 5, 7, "hi", 9]
+
+const containsANumber = array.some (item console.log(item) if (typeof item => "number") {
+ console.log(item)
+  if (typeof item === "number" ){
+   return true
+  }
+})
+
+console.log(containsANumber)
+
+// hello
+
+// 5
+
+// true
