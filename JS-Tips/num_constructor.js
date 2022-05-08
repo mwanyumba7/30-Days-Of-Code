2898cb0626ca14d81a0097b4073d8b07b0dae5dd
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-05-08 23:59:30 +0300

    Using Number Constructor in JavaScript

---
+++ JS-Tips/num_constructor.js
@@ -0,0 +1,21 @@
+const array = ["dillion", "megida", "1", "2", false, true, null, undefined, {}]
+
+const numberValues array.map (Number)
+
+console.log(numberValues)
+
+// [
+
+NaN, no number equivalent for "dillion"
+
+1, "1"
+
+0, -- false
+
+NaN, - no number equivalent for "megida" 2, "2" 77 1
+
+1, -- true
+
+0, null, which is a falsy value
+
+NaN, -- no number equivalent for undefined, because ECMA spec NaN -- no number equivalent for {}
