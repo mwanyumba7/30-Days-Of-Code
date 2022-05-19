d4715c6a58b2cc680d317a82f3de7e3eb0dd71c8
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-05-19 21:18:51 +0300

    js tip on how to escape strings

---
+++ JS-Tips/escaping_strings.js
@@ -0,0 +1,7 @@
+const joke = "Why couldn't the shoes go out and play? \nThey were all \"tied\" up!"
+console.log(joke);
+
+// Output
+
+/*Why couldn't the shoes go out and play?
+They were all "tied" up!*/
